8f277b2f12864c19fe7995aed9d515d4595bd561
homesangsang <243775257@qq.com>
2015-10-28 09:39:51 +0800

    finish a program linkedlist.c

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -25,8 +25,9 @@ int main()
 	printf(" Delete node (y/n):");
 	scanf("%s",num);
 	if(strcmp(num,"y")==0||strcmp(num,"Y")==0) head=AddNode(head);
-	printlist("pause");
-	DeleteAll(headheadheadheadheadheadheadheadhead);
+	printlist(head);
+	system("pause");
+	DeleteAll(head);
 }
 //用尾插入法建立带头节点的单链表
 LinkList CreatListR1()
@@ -57,7 +58,7 @@ LinkList CreatListR1()
 	return head;
 }
 //用头插入法建立带头节点的单链表
-List CreatList()
+LinkList CreatList()
 {
 	char ch[100];
 	LinkList head,p;
@@ -83,5 +84,59 @@ List CreatList()
 ListNode * LocateNode(LinkList head,char * key)
 {
 	ListNode * p=head->next;
-	while(p!=NULL)
+	while(p!=NULL&&strcmp(p->data,key)!=0)	p=p->next;
+	return p;
+}
+//修改程序：增加节点
+ListNode * AddNode(LinkList head)
+{
+	char ch[10];
+	ListNode * s,* pp;
+	printf("\nPlease input a New Node_data:");
+	scanf("%s",ch);
+	pp=LocateNode(head,ch);
+	if(pp==NULL){
+		s=(LinkList)malloc(sizeof(ListNode));
+		strcpy(s->data,ch);
+		s->next=head->next;
+		head->next=s;
+	}
+	return head;
+}
+//删除带头节点的单链表中的制定节点
+void DeleteList(LinkList head,char * key)
+{
+	ListNode *p,*r,*q=head;
+	p=LocateNode(head,key);
+	if(p==NULL)
+	{
+		printf("position error");
+		exit(0);
+	}
+	while(q->next!=p)	q=q->next;//p为要删除的节点，q为p的前节点。该行代码的目的是使q指针移动到p
+	r=q->next;
+	q->next=r->next;
+	free(r);
+}
+//打印链表
+void printlist(LinkList head)
+{
+	ListNode *p = head -> next;
+	while(p){
+		printf("%s,    ",p->data);
+		p=p->next;
+	}
+	printf("\n");
+}
+//删除所有节点，释放空间
+void DeleteAll(LinkList head)
+{
+	ListNode * p=head,*r;
+	while(p->next)
+	{
+		r=p->next;
+		free(p);
+		p=r;
+	}
+	free(p);
 }
